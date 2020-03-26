d1a0b5027d19841e41d39039143ea26eb66a84e9
Elias Taveras <eliastave12@gmail.com>
2020-03-26 02:08:45 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ end
 def print_first_directors_movie_titles
   pretty_print_nds(directors_database)
   
-  first_director = vm[0]
+  first_director = directors_database[0]
   movies = first_director[:movies]
   counter = 0
 
