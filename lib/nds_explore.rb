6a1393ac7aecb4c3af8cb10bec318f48ea730c05
Elias Taveras <eliastave12@gmail.com>
2020-03-26 02:17:59 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -17,7 +17,7 @@ def print_first_directors_movie_titles
 
   while counter < movies.length do
       movie = movies[counter]
-      pp movie[:title]
+      pp movie[:title] + "\n"
       counter += 1
   end
 end
